d56275651e7e33a027639491d4a7c127e9af3817
Bryan Reed <bryan87reed@gmail.com>
2021-12-18 09:56:13 -0700

    Small updates and more responses

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
+const getRandomResponse = () => {
+  const responses = [
+    `Nice Try.`,
+    `That message includes contraband.`,
+    `Really? You should know better.`,
+    `Oh nein, tust du nicht!`,
+  ];
+  const randomIndex = Math.floor(Math.random() * responses.length);
+  return responses[randomIndex];
+};
+
 const findThatPig = async (msg) => {
   const pigGifUrl = "https://tenor.com/view/gnome-squeeze-hog-gif-13302788";
   const pigGifBufferOne = readFileSync("pig.gif");
@@ -17,27 +28,36 @@ const findThatPig = async (msg) => {
   const savedPigGifArray = [pigGifBufferOne, pigGifBufferTwo];
 
   const attachmentPromises = msg.attachments.map((attachment) => {
+    // Downloads the message attachments and returns them as buffers
     return axios
       .get(attachment?.url, { responseType: "arraybuffer" })
       .then((res) => Buffer.from(res.data, "utf-8"));
   });
   const buffers = await Promise.all(attachmentPromises);
   const pigBufferMatches = buffers.filter((pg) => {
+    // Find any buffers that match the saved files
     return savedPigGifArray.find((spg) => Buffer.compare(spg, pg) === 0);
   });
 
+  // Find any attachments that have the common names of controband gifs
   const pigs = msg.attachments.filter(
-    (a) => a.name === "pig.gif" || a.name === "gnome-squeeze.png"
+    (a) => a.name === "pig.gif" || a.name === "gnome-squeeze.gif"
   );
 
   const notAllowed =
     pigs.size !== 0 ||
     pigBufferMatches.length !== 0 ||
-    msg.content === pigGifUrl;
+    msg.content.includes(pigGifUrl);
 
   if (notAllowed) {
-    msg.reply("Nice try.");
+    msg.reply(getRandomResponse());
     setTimeout(() => msg.delete(), 500);
+  }
+};
+
+const idiotPatrol = (msg) => {
+  if (msg.content.toLowerCase().includes(`idiot`)) {
+    msg.reply(`That's your one. Tread carefully`);
   }
 };
 
@@ -47,6 +67,8 @@ client.on("ready", () => {
 
 client.on("messageCreate", (msg) => {
   findThatPig(msg);
+  // ** To be determined what's to come **
+  // idiotPatrol(msg);
 });
 
 client.login(process.env.TOKEN);
